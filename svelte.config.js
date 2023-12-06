d4cca9bd5b1eabf4b9256d5beeb75af87a512877
PokeFred <Cedric.paelmke@gmx.de>
2023-12-06 13:17:15 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,10 @@ const config = {
         }),
         paths: {
             base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
+        },
+        alias: {
+            "$components": "./src/lib/components",
+            "$utils": "./src/lib/utils"
         }
     }
 }
